708aa05a239e2312d4774c8a39f2eb9bb1e59343
Azran Hossain <mr69751@gmail.com>
2023-03-07 10:53:20 +0600

    base template

---
+++ webapps/WEB-INF/classes/Controllers/Home.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        req.getRequestDispatcher("home.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
