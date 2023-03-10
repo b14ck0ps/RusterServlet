074954d3d449d2d0f24ffff6b4264062139e0cf4
Azran Hossain <mr69751@gmail.com>
2023-03-10 18:10:59 +0600

    admins routes

--- webapps/AddProduct.jsp
+++ webapps/AddProduct.jsp
@@ -1,15 +1,15 @@
-<%@include file="Header.jsp" %>
-<%@include file="AdminNavBar.jsp" %>
+<%@include  file="Header.jsp" %>
+<%@include  file="AdminNavBar.jsp" %>
 <main>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="card p-3 m-2">
-                    <h1>Admin Dashboard</h1>
-                    <p>This is the Admin Dashboard</p>
+                    <h1>Add New Product</h1>
+                    <p>This is the Cart Page</p>
                 </div>
             </div>
         </div>
     </div>
 </main>
-<%@include file="Footer.jsp" %>
+<%@include  file="Footer.jsp" %>
---
+++ webapps/WEB-INF/classes/Controllers/AddProduct.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AddProduct extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        req.getRequestDispatcher("AddProduct.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
