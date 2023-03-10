eda012729be990922b1c81d2df144b02d126a80b
Azran Hossain <mr69751@gmail.com>
2023-03-11 01:54:06 +0600

    Logout funcs

---
+++ webapps/WEB-INF/classes/Controllers/Logout.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Logout extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        req.getSession().invalidate();
+        resp.sendRedirect("Login");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
