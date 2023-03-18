ffbb52aa175283f50483283d50200ba26db544cf
Azran Hossain <mr69751@gmail.com>
2023-03-19 02:31:36 +0600

    DriverManager db connetion code removed

--- webapps/WEB-INF/classes/DBservices/DatabasesConnection.java
+++ webapps/WEB-INF/classes/DBservices/DatabasesConnection.java
@@ -17,14 +17,4 @@ public class DatabasesConnection {
         }
         return con;
     }
-//    public static Connection getConnection() {
-//        Connection con = null;
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rustershop", "root", "");
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return con;
-//    }
 }
